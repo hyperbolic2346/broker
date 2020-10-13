88042f19f1fb6858ed4e333907a1b507fc477da6
hyperbolic2346 <knobby2346+git@gmail.com>
2020-10-13 00:06:51 -0400

    pdo

--- index.php
+++ index.php
@@ -5,23 +5,13 @@ $username = getenv("DB_USER");
 $password = getenv("DB_PASSWORD");
 $database = getenv("DB_DATABASE");
 
-try{
-    $conn = new PDO("mysql:host=$servername;dbname=$database",$username,$password);
-    $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Connected succesfully";
- } catch(PDOException $e){
-    echo "Connection failed: " . $e -> getMessage();
- }
-
-// Create connection
-$db = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  $db = new PDO("mysql:host=$servername;dbname=$database",$username,$password);
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  echo "Connected to db succesfully<br />";
+} catch(PDOException $e){
+  die("DB connection failed: " . $e -> getMessage());
 }
-
-echo "Connected successfully<br />";
 
 // verify schema
 $tbls = $db->query("SELECT TABLES");
