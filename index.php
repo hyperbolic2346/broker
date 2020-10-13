d569ae692b5a58b481948957d3ccd172145d6478
hyperbolic2346 <knobby2346+git@gmail.com>
2020-10-12 23:30:32 -0400

    dding some db stuff

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
+// check db
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_DATABASE");
 
-echo "Hello world";
+// Create connection
+$db = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully<br />";
+
+// verify schema
+$tbls = $db->query("SELECT TABLES");
+if ($tbls->num_rows == 0) {
+  echo "initializing db<br />";
+}
+echo "Hello world!";
 
 ?>
